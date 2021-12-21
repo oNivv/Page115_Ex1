a00585b306d67dd00fd393a68d40891c5c563d71
oNivv <93833508+oNivv@users.noreply.github.com>
2021-12-21 09:41:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp72
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            A a = new B();
+
+        }
+
+        public class A
+        {
+            public A()
+            {
+                Console.WriteLine("A C'tor");
+                PrintMe();
+            }
+            public virtual void PrintMe()
+            {
+                Console.WriteLine("A print me");
+            }
+        }
+        public class B : A
+        {
+            public B()
+            {
+                Console.WriteLine("B C'tor");
+                PrintMe();
+            }
+            public override void PrintMe()
+            {
+                Console.WriteLine("B Print Me");
+            }
+        }
+    }
+}
